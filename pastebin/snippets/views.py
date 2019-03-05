de3c9fbcde5f024ecf1590c824ec662fcdb0b5ed
peterbristow <peter.bristow@fundingoptions.com>
2019-03-05 12:59:44 +0000

    Add Pastebin project

---
+++ pastebin/snippets/views.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer, UserSerializer
+from rest_framework import generics
+
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
